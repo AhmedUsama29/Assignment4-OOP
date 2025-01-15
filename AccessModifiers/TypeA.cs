27b2892a264c4498f571d5222a1173e8ca8be662
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-01-15 21:28:58 +0200

    Type A & B

---
+++ AccessModifiers/TypeA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    internal class TypeA
+    {
+        public TypeA(int a, int b, int c, int d, int e)
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+            E = e;
+        }
+
+        public int A { get; set; } //public (no inheretance) 
+
+        private int B { get; set; } // private (no inheretance)
+
+        protected int C { get; set; } //private (no inheretance)
+        private protected int D { get; set; } // private (no inheretance)
+        protected internal int E { get; set; } // internal (no inheretance)
+
+
+        public override string ToString()
+        {
+            return $"A = {A}, B = {B}, C = {C}, D = {D}, E = {E},";
+        }
+
+    }
+}
---
+++ AccessModifiers/TypeB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    internal class TypeB : TypeA
+    {
+
+        public int F { get; set; }
+        public int G { get; set; }
+
+        public TypeB(int a, int b, int c, int d, int e,int f, int g) :base(a,b,c,d,e)
+        {
+            F = f;
+            G = g;
+        }
+
+        override public string ToString()
+        {
+            return base.ToString() + $"F = {F}, G = {G}";
+
+        }
+    }
+}
