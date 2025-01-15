654bb17bfc99be78e4f280770b1870524aee4ce6
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-01-15 21:58:35 +0200

    Composition

---
+++ Assignment4-OOP/Association/Composition/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_OOP.Association.Composition
+{
+    //Association Realationship [Composition]
+    internal class OrderItem
+    {
+        public int id { get; set; }
+
+        public required Product Product { get; set; }
+
+        //public OrderItem(Product product)
+        //{
+        //    Product = product;
+        //}
+    }
+}
