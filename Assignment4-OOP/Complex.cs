68fbcf1cd0fed56ccea96194372d45b261a2dbee
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-01-15 22:32:53 +0200

    Operators

---
+++ Assignment4-OOP/Complex.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_OOP
+{
+    internal class Complex
+    {
+
+        public int Real { get; set; }
+
+        public int Imaginary { get; set; }
+
+        override public string ToString()
+        {
+            return $"{Real} + {Imaginary}i";
+        }
+
+        //OverLoading operator : Non-Private Class member function
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c = new Complex();
+            c.Real = (c1?.Real ?? 0) + (c2?.Real ?? 0);
+            c.Imaginary = (c1?.Imaginary ?? 0) + (c2?.Imaginary ?? 0);  
+            return c;
+        }
+
+        public static Complex operator +(Complex c1, int right)
+        {
+            Complex c = new Complex();
+            c.Real = (c1?.Real ?? 0) + right;
+            c.Imaginary = (c1?.Imaginary ?? 0) + right;
+            return c;
+        }
+        public static Complex operator +(int left, Complex c2)
+        {
+            Complex c = new Complex();
+            c.Real = left + (c2?.Real ?? 0);
+            c.Imaginary = left + (c2?.Imaginary ?? 0);
+            return c;
+        }
+
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            Complex c = new Complex();
+            c.Real = (c1?.Real ?? 0) - (c2?.Real ?? 0);
+            c.Imaginary = (c1?.Imaginary ?? 0) - (c2?.Imaginary ?? 0);
+            return c;
+        }
+
+        //unary operator
+
+        public static Complex operator ++(Complex c1)
+        {
+            Complex c = new Complex();
+            c.Real = (c1?.Real ?? 0) + 1;
+            c.Imaginary = c1.Imaginary;
+            return c;
+        }
+        public static Complex operator --(Complex c1)
+        {
+            Complex c = new Complex();
+            c.Real = (c1?.Real ?? 0) - 1;
+            c.Imaginary = c1.Imaginary;
+            return c;
+        }
+
+        // < > <= >= == !=
+
+        public static bool operator >(Complex c1, Complex c2)
+        {
+            if(c1?.Real == c2?.Real)
+                return c1?.Imaginary > c2?.Imaginary;
+            else return c1?.Real > c2?.Real;
+        }
+
+        public static bool operator <(Complex c1, Complex c2)
+        {
+            if (c1?.Real == c2?.Real)
+                return c1?.Imaginary < c2?.Imaginary;
+            else return c1?.Real < c2?.Real;
+        }
+
+
+    }
+}
--- Assignment4-OOP/Program.cs
+++ Assignment4-OOP/Program.cs
@@ -4,35 +4,129 @@ namespace Assignment4_OOP
 {
     internal class Program
     {
+
+        #region Polymorphism-overloading
+        //static int Sum02Numbers(int a, int b)
+        //{
+        //    return a + b;
+        //}
+        //static int Sum03Numbers(int a, int b,int c)
+        //{
+        //    return a + b + c;
+        //}
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static int Sum(int a, int b, int c, int d)
+        {
+            return a + b + c + d;
+        } 
+        #endregion
+
         static void Main(string[] args)
         {
+            #region Inheritance and realationships between classes
             /*////////////////////////////////////////////////////////////////////
-            Console.WriteLine("============Parent============");
-            Parent parent = new Parent(1, 2);
+                Console.WriteLine("============Parent============");
+                Parent parent = new Parent(1, 2);
 
-            parent.X = 10;
-            parent.Y = 20;
+                parent.X = 10;
+                parent.Y = 20;
 
-            Console.WriteLine(parent);
-            Console.WriteLine(parent.Product());
+                Console.WriteLine(parent);
+                Console.WriteLine(parent.Product());
 
-            Console.WriteLine("============Child============");
+                Console.WriteLine("============Child============");
 
-            Child child = new Child(1, 2, 5);
+                Child child = new Child(1, 2, 5);
 
-            Console.WriteLine(child);
-            Console.WriteLine(child.Product());
+                Console.WriteLine(child);
+                Console.WriteLine(child.Product());
 
-            //Animal = Parent
-            //Dog = Child
-            //Cat = Child
-            /////////////////////////////////////////////////////////////////////////*/
-            
+                //Animal = Parent
+                //Dog = Child
+                //Cat = Child
+                /////////////////////////////////////////////////////////////////////////*/
+
             //Car car = new Car(1,2,3,4,5,"Toyota", 2020, 200);
 
             //Console.WriteLine(car);
 
-            Product P = new Product() { Name = "Coffee"};
+            //Product P = new Product() { Name = "Coffee"}; 
+            #endregion
+
+            #region Polymorphosim--overloading
+
+            #region Method Overloading
+            ////int result = Sum02Numbers(1, 2);
+            //int result = Sum(1, 2);
+            //Console.WriteLine(result);
+
+            //result = Sum(1, 2, 6);
+            //Console.WriteLine(result);
+
+            //result = Sum(1, 2, 2, 8);
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Operator Overloading
+
+            Complex c1 = new Complex();
+            c1.Real = 5;
+            c1.Imaginary = 3;
+            Console.WriteLine($"Complex 1 ===> {c1}");
+            Complex c2 = new Complex();
+            c2.Real = 2;
+            c2.Imaginary = 4;
+            Console.WriteLine($"Complex 2 ===> {c2}");
+
+            Complex c3 = c1 + c2;
+            Console.WriteLine($"Complex 3 ===> {c3}");
+
+            Complex c4 = c1 + 5;
+
+            Console.WriteLine($"Complex 4 ===> {c4}");
+
+            Complex c5 = 5 + c2;
+
+            Console.WriteLine($"Complex 5 ===> {c5}");
+
+            Console.WriteLine("-------------------unary------------------");
+            #region Unary operators
+
+            Complex c6 = c5++;
+
+            Console.WriteLine($"Complex 6 ===> {c6}");
+
+            Complex c7 = ++c6;
+
+            Console.WriteLine($"Complex 7 ===> {c7}");
+
+
+            Console.WriteLine("----------<&>-------------");
+
+            if (c1 > c2)
+            {
+                Console.WriteLine("c1 > c2");
+            }
+            else
+            {
+                Console.WriteLine("c1 < c2");
+            }
+
+            #endregion
+
+            #endregion
+
+            #endregion
         }
     }
 }
